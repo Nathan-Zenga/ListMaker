2fa4c785ef6c2bf1d63f96cbc4a5a9c0ef98b30d
Nathan Zengamambu <zengaman@ecm15-mac-18.coventry.ac.uk>
2017-11-06 15:13:39 +0000

    Initial Commit

---
+++ ListMaker/ListMaker.h
@@ -0,0 +1,19 @@
+//
+//  ListMaker.h
+//  ListMaker
+//
+//  Created by Nathan Zengamambu on 06/11/2017.
+//  Copyright © 2017 Nathan Zengamambu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ListMaker.
+FOUNDATION_EXPORT double ListMakerVersionNumber;
+
+//! Project version string for ListMaker.
+FOUNDATION_EXPORT const unsigned char ListMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListMaker/PublicHeader.h>
+
+
